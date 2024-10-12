f528f18ea9d9835a2420f63f164597b9fb17bf97
Francisco José Fernández García <francisco.fernandez@terraminium.com>
2024-10-12 18:16:03 +0200

    updated exercise 3

--- 01-entorno-linux/Exercise-3/esercise3.sh
+++ 01-entorno-linux/Exercise-3/esercise3.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
-TEXT="Que me gusta la bash!!"
-
-if [[ $# -ge 1 ]]; then
-        TEXT=$1
-fi
-
 rm -r foo/ 2> /dev/null
 
 mkdir -p foo/dummy
 mkdir -p foo/empty
 
-echo $TEXT > foo/dummy/file1.txt | touch foo/dummy/file2.txt
+echo ${1:-'Que te gusta la bash!!'} > foo/dummy/file1.txt | touch foo/dummy/file2.txt
 cat foo/dummy/file1.txt > foo/dummy/file2.txt
 
 mv foo/dummy/file2.txt foo/empty/
